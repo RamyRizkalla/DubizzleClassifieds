94333119f657ef4a59c5802669404785462de6ed
Ramy Rizkalla <rwagdyrizkalla@gmail.com>
2021-09-30 23:59:28 +0200

    Initial Commit

---
+++ Classifieds/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Classifieds
+//
+//  Created by Ramy Wagdy on 9/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
